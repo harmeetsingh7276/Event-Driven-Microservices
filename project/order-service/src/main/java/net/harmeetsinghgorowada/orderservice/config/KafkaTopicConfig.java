38a7ed2357037fe508e62c3d30b1748225b19b96
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-07-22 15:43:08 +0530

    added orderproducer class and it's configs

--- project/order-service/src/main/java/net/harmeetsinghgorowada/orderservice/config/KafkaTopicConfig.java
+++ project/order-service/src/main/java/net/harmeetsinghgorowada/orderservice/config/KafkaTopicConfig.java
@@ -2,6 +2,7 @@ package net.harmeetsinghgorowada.orderservice.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
@@ -11,6 +12,7 @@ public class KafkaTopicConfig {
     private String topicName;
 
     //spring bean for kafka topic
+    @Bean
     public NewTopic topic() {
         return TopicBuilder.name(topicName).build();
     }
